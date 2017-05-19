08a5ff20ee9aa787688a2b6e5d05cafc3d7e29b3
kenkina <ken.kina97@gmail.com>
2017-05-19 01:33:19 -0500

    Login: connect to SignUp NoFuncional

--- app/src/main/java/com/whisper/whispme/activities/LoginActivity.java
+++ app/src/main/java/com/whisper/whispme/activities/LoginActivity.java
@@ -1,15 +1,78 @@
 package com.whisper.whispme.activities;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.whisper.whispme.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputEditText emailTextInputEditText, passwordInputEditText;
+    TextView createAccountTextView;
+    Button loginButton, loginFacebookButton, loginGoogleButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailTextInputEditText = (TextInputEditText) findViewById(R.id.emailTextInputEditText);
+        passwordInputEditText = (TextInputEditText) findViewById(R.id.passwordInputEditText);
+
+        createAccountTextView = (TextView) findViewById(R.id.createAccountTextView);
+        createAccountTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        new Intent(LoginActivity.this,
+                                SignUpActivity.class));
+                finish();
+            }
+        });
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        new Intent(LoginActivity.this,
+                                SignUpActivity.class));
+                finish();
+            }
+        });
+
+        loginFacebookButton = (Button) findViewById(R.id.loginFacebookButton);
+        loginFacebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        new Intent(LoginActivity.this,
+                                SignUpActivity.class));
+                finish();
+            }
+        });
+
+        loginGoogleButton = (Button) findViewById(R.id.loginGoogleButton);
+        loginGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(
+                        new Intent(LoginActivity.this,
+                                SignUpActivity.class));
+                finish();
+            }
+        });
+
+
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/whisper/whispme/activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.whisper.whispme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.whisper.whispme.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
